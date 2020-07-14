3de6a84c738e0e82f421ef37ad4dc0341704d327
Jack Thake <jackthake@hotmail.com>
2020-07-13 21:24:54 -0700

    Added in tokenizer, needs finishing up

--- cs162/prog2/main.cpp
+++ cs162/prog2/main.cpp
@@ -14,25 +14,18 @@
 
 using namespace std;
 
+const int MAX_WORD = 21;
 
+struct word {
+  int start_pos, end_pos;
+  char data[MAX_WORD];
+};
 
 /*
  * Function prototypes
 */
 void read_into_buffer(const char prompt[], char response[], int response_size);
-
-
-
-int main(void) {
-  char buf[21];
-
-  read_into_buffer("Enter something: ", buf, 21);
-  cout << buf << endl;
-
-  return EXIT_SUCCESS;
-}
-
-
+void tokenize_string(const char str[], int str_size, word tokens[]);
 
 /*
  * Function definitions
@@ -41,7 +34,7 @@ int main(void) {
 /*
  * read_into_buffer(const char prompt[], char response[], int response_size)
  *
- * breif: prints the characters in the prompt array, and awaits the users input,
+ * brief: prints the characters in the prompt array, and awaits the users input,
  *        clearing the input buffer after the appropriate amount of input is collected
  * prompt: array of characters to be displayed to the user
  * response: array of characters that will be modified to hold the user's
@@ -53,3 +46,55 @@ void read_into_buffer(const char prompt[], char response[], int response_size) {
   cin.get(response, response_size, '\n');
   cin.ignore(100, '\n');
 }
+
+/*
+ * tokenize_string(const char str[], int str_size, word tokens[])
+ *
+ * brief: takes a string, splits it into a list of words
+ * str[]: the string to be split into words
+ * str_size: length of string not including terminating null
+ * tokens[]: an array of word structs each containing one of the words in the
+ *           passed string
+*/
+void tokenize_string(const char str[], int str_size, word tokens[]) {
+  int current_word = 0;
+  bool word_started = false;
+
+  for(int i = 0; i < str_size; ++i) {
+    if(!isblank(str[i]))  {  /* Don't care about spaces or blank characters  */ 
+      if((i == 0 || isblank(str[i - 1])) && !word_started) { /* find the first character of a word, the first character will always be treated as the start of the first word */
+        tokens[current_word].start_pos = i;
+        word_started = true;
+      }
+
+      if(i == str_size - 1 || isblank(str[i + 1])) { /* find the end of the word, respecting the array bounds  */
+        tokens[current_word].end_pos = i;
+        
+        /* TODO: copy sub string */
+
+        /* get ready for the next word */
+        word_started = false;
+        ++current_word;
+      }
+    }
+  }
+}
+
+int main(void) {
+  /* test code */
+  char buf[48];
+  word tokens[12];
+  
+  read_into_buffer("Enter something: ", buf, 48);
+  cout << buf << endl;
+  cout << strlen(buf) << endl;
+
+  tokenize_string(buf, strlen(buf), tokens);
+
+  for(int i = 0; i < 12; i++) {
+    cout << tokens[i].start_pos << " " << tokens[i].end_pos << endl;
+  }
+
+  return EXIT_SUCCESS;
+}
+
