5a5043c3a6a8b2e66f731b294d2076aeabba080d
Jack Thake <jackthake@hotmail.com>
2020-07-18 13:52:59 -0700

    new approach to solving the problem, much simpler

--- cs162/prog2/main.cpp
+++ cs162/prog2/main.cpp
@@ -1,107 +1,119 @@
-/*
- * Puzzle Game
- * Jack Thake
- * CS162, Program 2
- * July 8th, 2020
- *
- * I'm trying out underscores rather than camel case
- * Still coded only in vim :D
-*/
-
 #include <iostream>
-#include <cctype>
+#include <ctype.h>
 #include <cstring>
-
+#include <time.h>
 using namespace std;
 
-const int MAX_WORD = 21;
+/* prototypes */
+void prompt_user_input(const char *prompt, char *response, unsigned response_length);
+void clear_term();
+void embed_word(const char *word, char *phrase);
+char rand_alpha();
 
-struct word {
-  int start_pos, end_pos;
-  char data[MAX_WORD];
-};
+/* constants */
+const unsigned SECRET_LENGTH = 11;
+const unsigned PHRASE_LENGTH = 101;
+
+int main(int argc, char **argv) {
+  /* initialise secret_word and phrase */
+  char secret_word[SECRET_LENGTH];
+  char phrase[PHRASE_LENGTH];
+
+  /* read in the phrase and word */
+  prompt_user_input("Please enter the secret word: ", secret_word, SECRET_LENGTH);
+  prompt_user_input("Please enter the phrase: ", phrase, PHRASE_LENGTH);
+
+  /* clear screen */
+  clear_term();
+
+  /* print out the phrase and word */
+  cout << secret_word << endl;
+  cout << phrase << endl;
+
+  embed_word(secret_word, phrase);
+
+  cout << phrase << endl;
+
+  return 0;
+}
+
+
 
 /*
- * Function prototypes
-*/
-void read_into_buffer(const char prompt[], char response[], int response_size);
-void tokenize_string(const char str[], int str_size, word tokens[]);
-
-/*
- * Function definitions
-*/
-
-/*
- * read_into_buffer(const char prompt[], char response[], int response_size)
+ * void prompt_user_input(const char *prompt, char *response, unsigned response_length)
  *
- * brief: prints the characters in the prompt array, and awaits the users input,
- *        clearing the input buffer after the appropriate amount of input is collected
- * prompt: array of characters to be displayed to the user
- * response: array of characters that will be modified to hold the user's
- *           response
- * response_size: holds the size of the response array
+ * brief: prompts user for input, reads in the entered text and stores it.
+ *
+ * prompt: text to be printed to the user
+ * response: location where the user's response will be stored
+ * response_length: length of response's memory block
 */
-void read_into_buffer(const char prompt[], char response[], int response_size) {
+void prompt_user_input(const char *prompt, char *response, unsigned response_length) {
   cout << prompt;
-  cin.get(response, response_size, '\n');
+
+  cin.width(response_length);
+  cin.get(response, response_length, '\n');
   cin.ignore(100, '\n');
 }
 
+
+
 /*
- * tokenize_string(const char str[], int str_size, word tokens[])
+ * void clear_term()
  *
- * brief: takes a string, splits it into a list of words
- * str[]: the string to be split into words
- * str_size: length of string not including terminating null
- * tokens[]: an array of word structs each containing one of the words in the
- *           passed string
+ * brief: clears the terminal screen in a non-platform specific way.
 */
-void tokenize_string(const char str[], int str_size, word tokens[]) {
-  int current_word = 0;
-  bool word_started = false;
+void clear_term() {
+  for (int i = 0; i < 200; ++i) {
+    cout << '\n';
+  }
 
-  for(int i = 0; i < str_size; ++i) {
-    if(!isblank(str[i]))  {  /* Don't care about spaces or blank characters  */ 
-      if((i == 0 || isblank(str[i - 1])) && !word_started) { /* find the first character of a word, the first character will always be treated as the start of the first word */
-        tokens[current_word].start_pos = i;
-        word_started = true;
-      }
+  cout << endl;
+}
 
-      if(i == str_size - 1 || isblank(str[i + 1])) { /* find the end of the word, respecting the array bounds  */
-        tokens[current_word].end_pos = i;
-        
-        /* 
-         * Copy the word into the word struct
-        */
-        for(int j = 0; j <= tokens[current_word].end_pos - tokens[current_word].start_pos; ++j) {
-          tokens[current_word].data[j] = str[tokens[current_word].start_pos + j];
+
+
+void embed_word(const char *word, char *phrase) {
+  int counter = 0; /* holds number of characters since the last space */
+  int cur_word_char = 0; /* holds the current character needing to be embedded */
+  bool has_word_editted = false;
+
+  for (int i = 0; i < strlen(phrase); ++i) {
+    if(isspace(phrase[i])) {
+      counter = 0;
+      has_word_editted = false;
+    } else {
+      if(counter == cur_word_char && !has_word_editted) {
+        if(!isupper(phrase[i])) {
+          phrase[i] = word[cur_word_char];
+          ++cur_word_char;
+          has_word_editted = true;
         }
-
-        /* add the terminating null character  */
-        tokens[current_word].data[tokens[current_word].end_pos - tokens[current_word].start_pos + 1] = '\0';
-
-        /* get ready for the next word */
-        word_started = false;
-        ++current_word;
       }
+
+      ++counter;
     }
+
+    if(phrase[i] == '_')
+      phrase[i] = rand_alpha();
   }
 }
 
-int main(void) {
-  /* test code */
-  char buf[48];
-  word tokens[12];
-  
-  read_into_buffer("Enter something: ", buf, 48);
-  cout << buf << endl;
 
-  tokenize_string(buf, strlen(buf), tokens);
 
-  for(int i = 0; i < 12; i++) {
-    cout << tokens[i].start_pos << " " << tokens[i].end_pos << " " << tokens[i].data << endl;
-  }
+/*
+ * char rand_alpha()
+ *
+ * brief: returns a random lowercase letter
+ *
+ * return: random lowercase letter
+*/
+char rand_alpha() {
+  srand(time(NULL));
 
-  return EXIT_SUCCESS;
+  /*
+   * we use this formula to create a range, from 97 ('a') to 122 ('z') on the
+   * ascii table.
+  */
+  return (rand() % (122 - 97 + 1)) + 97;
 }
-
