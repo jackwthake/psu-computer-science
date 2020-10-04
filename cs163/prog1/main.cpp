4bb617286f47085cc20f44278a986f52ae990504
Jack Thake <jackthake@hotmail.com>
2020-10-03 19:07:30 -0700

    :tada: Started work on program 1's source

---
+++ cs163/prog1/main.cpp
@@ -0,0 +1,12 @@
+/*
+ * Jack Thake, CS163, 10.3.20
+ * This file holds the test program for this assignment.
+*/
+
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+  std::cout << "Hello world" << std::endl;
+
+  return 0;
+}
