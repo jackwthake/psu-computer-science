529a9912e4bdd0b5ef48c01ff353b1c9c3076002
Jack Thake <jackthake@hotmail.com>
2020-11-11 10:48:33 -0800

    :bulb: Added comments for CS_item_queue.

--- cs163/prog3/CS_item_queue.cpp
+++ cs163/prog3/CS_item_queue.cpp
@@ -1,17 +1,29 @@
+//
+//  CS_item_queue.cpp
+//  psu-computer-science
+//
+//  Created by Jack Thake on 11/11/2020.
+//  This file holds the implementation of the CS_item_queue class
+//
+
 #include "CS_item_queue.h"
 #include <cstddef>
 #include <iostream>
 using namespace std;
 
+/* initialise queue */
 CS_item_queue::CS_item_queue() {
   this->qptr = NULL;
 }
 
 
+/* deallocate */
 CS_item_queue::~CS_item_queue() {
+  /* uncircularise the list - makes it easier to deallocate */
   node *head = this->qptr->next;
   this->qptr->next = NULL;
 
+  /* deallocate as if it were a LLL */
   while (head) {
     node *temp = head;
     head = head->next;
@@ -21,8 +33,9 @@ CS_item_queue::~CS_item_queue() {
 }
 
 
+/* add to the rear of the queue */
 CS_error CS_item_queue::enqueue(const CS_item_info &to_add) {
-  if (!this->qptr) {
+  if (!this->qptr) { /* empty queue */
     this->qptr = new node;
     this->qptr->data = to_add;
     this->qptr->next = this->qptr;
@@ -30,9 +43,10 @@ CS_error CS_item_queue::enqueue(const CS_item_info &to_add) {
     return SUCCESS;
   }
 
+  /* queue has atleast one item in it */
   node *temp = this->qptr->next;
   this->qptr->next = new node;
-  this->qptr = this->qptr->next;
+  this->qptr = this->qptr->next; /* we advance the rear of the queue */
 
   this->qptr->data = to_add;
   this->qptr->next = temp;
@@ -41,35 +55,39 @@ CS_error CS_item_queue::enqueue(const CS_item_info &to_add) {
 }
 
 
+/* remove from front of the queue, giving back the previous front of list */
 CS_error CS_item_queue::dequeue(CS_item_info &result) {
-  if (!this->qptr)
+  if (!this->qptr) /* empty queue, nothing to remove */
     return FAILURE;
 
-  node *temp = this->qptr->next->next;
-  result = this->qptr->next->data;
-  delete this->qptr->next;
-  this->qptr->next = temp;
+  node *temp = this->qptr->next->next; /* grab the new front of queue */
+  result = this->qptr->next->data; /* give back the front of the queue */
+  delete this->qptr->next; /* remove */
+  this->qptr->next = temp; /* move the front */
 
   return SUCCESS;
 }
 
 
+/* gives back the front of the queue */
 CS_error CS_item_queue::peak(CS_item_info &result) const {
-  if (!this->qptr)
+  if (!this->qptr) /* empty queue */
     return FAILURE;
 
+  /* give back result */
   result = this->qptr->next->data;
   return SUCCESS;
 }
 
 
+/* handler function for recursive print */
 CS_error CS_item_queue::display() const {
-  if (!this->qptr)
+  if (!this->qptr) /* empty queue */
     return FAILURE;
-  
-  if (this->qptr == this->qptr->next) {
+
+  if (this->qptr == this->qptr->next) { /* only one item in queue */
     this->qptr->data.print();
-  } else {
+  } else { /* queue has atleast 2 items, display them all */
     this->display_recurs(this->qptr->next);
   }
 
@@ -77,8 +95,9 @@ CS_error CS_item_queue::display() const {
 }
 
 
+/* recursive function to print each item in the queue */
 void CS_item_queue::display_recurs(node *current) const {
-  if (current == this->qptr) {
+  if (current == this->qptr) { /* last item in the queue */
     cout << "---------------------------------------" << endl;
     current->data.print();
     cout << "---------------------------------------" << endl;
@@ -86,6 +105,7 @@ void CS_item_queue::display_recurs(node *current) const {
     return;
   }
   
+  /* print current data and move to next iteration */
   cout << "---------------------------------------" << endl;
   current->data.print();
 
--- cs163/prog3/CS_item_queue.h
+++ cs163/prog3/CS_item_queue.h
@@ -1,3 +1,11 @@
+//
+//  CS_item_queue.h
+//  psu-computer-science
+//
+//  Created by Jack Thake on 11/11/2020.
+//  This file holds the declarations for the CS_item_queue class
+//
+
 #ifndef CS_ITEM_QUEUE
 #define CS_ITEM_QUEUE
 
@@ -6,14 +14,15 @@
 
 class CS_item_queue {
   public:
-    CS_item_queue();
-    ~CS_item_queue();
+    CS_item_queue(); /* construct qeue */
+    ~CS_item_queue(); /* deallocate */
 
-    CS_error enqueue(const CS_item_info &);
-    CS_error dequeue(CS_item_info &);
-    CS_error peak(CS_item_info &) const;
-    CS_error display() const;
+    CS_error enqueue(const CS_item_info &); /* add to queue */
+    CS_error dequeue(CS_item_info &); /* remove from queue storing what was removed */
+    CS_error peak(CS_item_info &) const; /* store front of queue */
+    CS_error display() const; /* print to user */
 
+    /* check if queue is empty */
     inline bool is_empty() const { return !qptr; }
   private:
     struct node {
@@ -21,8 +30,8 @@ class CS_item_queue {
       node *next;
     };
 
-    void display_recurs(node *current) const;
-    node *qptr;
+    void display_recurs(node *current) const; /* recursively print */
+    node *qptr; /* rear of queue */
 };
 
 #endif
