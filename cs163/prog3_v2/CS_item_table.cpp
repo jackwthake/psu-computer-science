c5c1b92a0310f0ffc5695582bb677307c114123c
Jack Thake <jackthake@hotmail.com>
2020-11-18 10:23:27 -0800

    :bulb: Added comments for CS_item_table class

--- cs163/prog3_v2/CS_item_table.cpp
+++ cs163/prog3_v2/CS_item_table.cpp
@@ -1,27 +1,37 @@
+/*
+ * This file holds all implementations for the CS_item_table class
+*/
+
 #include "CS_item_table.h"
 #include <cstring>
 #include <fstream>
 #include <iostream>
 using namespace std;
 
+/* Construct a table from a file */
 CS_item_table::CS_item_table(const char *fp, unsigned length) {
-  this->table = new node*[length];
-  this->length = length;
+  this->table = new node*[length]; /* allocate table */
+  this->length = length; /* set table length */
 
-  for (int i = 0; i < length; ++i)
+  for (int i = 0; i < length; ++i) /* 0 out table */
     this->table[i] = NULL;
 
-  ifstream stream;
-  stream.open(fp);
-  if (!stream) return;
+  ifstream stream; /* start file loading process */
+  stream.open(fp); /* load file */
+  if (!stream) return; /* only continue if the file exists */
 
   do {
     CS_item_info tmp(stream);
-    if (tmp.is_valid())
+    if (tmp.is_valid()) /* don't add an empty object */
       this->add_item(tmp);
-  } while (!stream.eof());
+  } while (!stream.eof()); /* loop until no more to add */
+
+  /* cleanup */
+  stream.close();
 }
 
+
+/* deallocate */
 CS_item_table::~CS_item_table(void) {
   /* deallocate each chain */
   this->remove_all();
@@ -30,13 +40,17 @@ CS_item_table::~CS_item_table(void) {
   delete []this->table;
 }
 
-CS_error CS_item_table::add_item(const CS_item_info &to_add) {
-  char *key = to_add.get_name();
-  unsigned hash = this->hash_function(key);
-  delete []key;
 
+/* add an item to the table */
+CS_error CS_item_table::add_item(const CS_item_info &to_add) {
+  char *key = to_add.get_name(); /* get a copy of the item's name */
+  unsigned hash = this->hash_function(key); /* get hash for item */
+  delete []key; /* we don't need the key anymore */
+
+  /* grab old first value */
   node *temp = this->table[hash];
 
+  /* bring in new data */
   this->table[hash] = new node;
   this->table[hash]->data = to_add;
   this->table[hash]->next = temp;
@@ -45,13 +59,15 @@ CS_error CS_item_table::add_item(const CS_item_info &to_add) {
 }
 
 
+/* remove specific item from the table */
 CS_error CS_item_table::remove_item(const char *key) {
-  unsigned hash = this->hash_function(key);
-  node *current = this->table[hash], *prev = NULL;
+  unsigned hash = this->hash_function(key); /* get the hash of the key */
+  node *current = this->table[hash], *prev = NULL; /* pointers needed for traversal */
 
+  /* empty chain */
   if (!current)
     return FAILURE;
-  else if (current->data == key) {
+  else if (current->data == key) { /* first item in chain is a match */
     node *temp = current;
     current = current->next;
     delete temp;
@@ -59,14 +75,16 @@ CS_error CS_item_table::remove_item(const char *key) {
     return SUCCESS;
   }
 
+  /* loop through rest of chain */
   while (current) {
-    if (current->data == key) {
-      prev->next = current->next;
+    if (current->data == key) { /* use the overloaded equality operator */
+      prev->next = current->next; /* delink node and deallocate */
       delete current;
 
       return SUCCESS;
     }
 
+    /* move forward */
     prev = current;
     current = current->next;
   }
@@ -75,6 +93,7 @@ CS_error CS_item_table::remove_item(const char *key) {
 }
 
 
+/* clear list */
 CS_error CS_item_table::remove_all(void) {
   if (!this->table) /* empty table */
     return FAILURE;
@@ -97,54 +116,64 @@ CS_error CS_item_table::remove_all(void) {
 }
 
 
+/* retrieve a specific item from the table */
 CS_error CS_item_table::retrieve(const char *key, CS_item_info &result) {
-  unsigned hash = this->hash_function(key);
-  node *current = this->table[hash];
+  unsigned hash = this->hash_function(key); /* get the hash of the key */
+  node *current = this->table[hash]; /* grab the pointer for traversal */
 
-  while (current) {
-    if (current->data == key) {
+  while (current) { /* traverse */
+    if (current->data == key) { /* found a match */
       result = current->data;
       return SUCCESS;
     }
 
+    /* prep for next traversal */
     current = current->next;
   }
 
+  /* if we reach here nothing was found */
   return FAILURE;
 }
 
 
+/* print all chains recursively */
 CS_error CS_item_table::print_all(void) const {
-  if (!this->table)
+  if (!this->table) /* nothing to print */
     return FAILURE;
 
+  /* loop through chains */
   for (int i = 0; i < this->length; ++i) {
-    cout << "Chain " << i + 1 << endl;
+    cout << "Chain " << i + 1 << endl; /* print markers */
     cout << "=====================================" << endl;
-    print_chain_recurs(this->table[i], true);
+    print_chain_recurs(this->table[i], true); /* fire off recursive function to print current chain */
   }
 
   cout << "=====================================" << endl;
 }
 
+
+/* private recursive funciton to print a chain */
 void CS_item_table::print_chain_recurs(node *head, bool is_first) const {
-  if (!head)
+  if (!head) /* nothing to print */
     return;
 
-  if (!is_first)
+  if (!is_first) /* just for prettier printing */
     cout << endl;
 
-  head->data.print();
-  this->print_chain_recurs(head->next, false);
+  head->data.print(); /* print data */
+  this->print_chain_recurs(head->next, false); /* continue recursive print */
 }
 
+
+/* hahs function used for hashing */
 unsigned CS_item_table::hash_function(const char *key) const {
-  if (!key || !(*key))
+  if (!key || !(*key)) /* nothing to hash - NULL pointer or empty key */
     return 0;
 
   unsigned hash = 0;
   for (int i = 0; i < strlen(key); ++i)
-    hash += i * key[i];
+    hash += i * key[i]; /* hash */
 
-  return hash % this->length;
+  return hash % this->length; /* wrap to array length */
 }
+
--- cs163/prog3_v2/CS_item_table.h
+++ cs163/prog3_v2/CS_item_table.h
@@ -1,3 +1,8 @@
+/*
+ * This file holds the declaration for the CS_item_table class, this class
+ * implements a hash table, and loads it's contents from a file
+*/
+
 #ifndef CS_ITEM_TABLE_H
 #define CS_ITEM_TABLE_H
 
@@ -6,14 +11,14 @@
 
 class CS_item_table {
   public:
-    CS_item_table(const char *, unsigned length = 7);
-    ~CS_item_table(void);
+    CS_item_table(const char *, unsigned length = 7); /* load from file */
+    ~CS_item_table(void); /* deallocate */
 
-    CS_error add_item(const CS_item_info &);
-    CS_error remove_item(const char *);
-    CS_error remove_all(void);
-    CS_error retrieve(const char *, CS_item_info &);
-    CS_error print_all(void) const;
+    CS_error add_item(const CS_item_info &); /* add item to hash table */
+    CS_error remove_item(const char *); /* remove a specific item */
+    CS_error remove_all(void); /* clear the list */
+    CS_error retrieve(const char *, CS_item_info &); /* retrieve a specific item */
+    CS_error print_all(void) const; /* print list, wrapper for recursive function */
   private:
     struct node {
       CS_item_info data;
@@ -23,8 +28,8 @@ class CS_item_table {
     node **table;
     unsigned length;
   private:
-    unsigned hash_function(const char *) const;
-    void print_chain_recurs(node *, bool) const;
+    unsigned hash_function(const char *) const; /* hash function used */
+    void print_chain_recurs(node *, bool) const; /* recursive funciton to print chain contents */
 };
 
 #endif
