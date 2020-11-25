21f062c2f1a4b8f8a0addf2e7d23427fcca1046d
Jack Thake <jackthake@hotmail.com>
2020-11-25 10:11:36 -0800

    :package: started work on test program

--- cs163/prog4/main.cpp
+++ cs163/prog4/main.cpp
@@ -1,24 +1,100 @@
 #include "video_entry.h"
 #include "video_tree.h"
 #include <iostream>
+#include <cstring>
 using namespace std;
 
+void read_in_field(const char *prompt, char *res, int len) {
+  cout << prompt;
+  cin.get(res, len, '\n');
+  cin.ignore(100, '\n');
+}
+
+
+void read_in_entry(video_tree &tree) {
+  video_entry entry;
+  char media[51], course[51], desc[51], next[51];
+  int length = 0;
+
+  read_in_field("Please enter the media name: ", media, 51);
+  read_in_field("Please enter the course name: ", course, 51);
+  read_in_field("Please enter the description: ", desc, 51);
+  read_in_field("Please enter the next item in the playlist: ", next, 51);
+  
+  cout << "Please enter the video's length in minutes: ";
+  cin >> length;
+  cin.ignore(100, '\n');
+
+  video_entry tmp(course, media, desc, length, next);
+  tree.insert(tmp);
+}
+
+
 int main(void) {
-  video_entry entry("class", "5", "discription", 5, "next");
-  video_entry entry2("CLASS2", "2", "discription", 5, "next");
-  video_entry entry3("CLASS2", "8", "discription", 5, "next");
-  video_entry entry4("CLASS2", "6", "discription", 5, "next");
   video_tree tree;
+  char conf = '\0';
 
+  cout << "Start of insertion loop" << endl;
 
-  tree.insert(entry);
-  tree.insert(entry2);
-  tree.insert(entry3);
-  tree.insert(entry4);
-  tree.insert(entry);
+  do {
+    cout << endl << endl;
+    read_in_entry(tree);
 
-  tree.remove_course("CLASS2");
+    cout << "More to add? (y/n): ";
+    cin >> conf; 
+    cin.ignore(100, '\n');
+  } while (tolower(conf) != 'n');
+
+  cout << "Now printing tree" << endl;
   tree.display_all();
+  cout << "Tree Height: " << tree.get_height() << endl;
+
+  cout << "Start of display" << endl;
+
+  do {
+    char search_name[51];
+    video_entry *arr = NULL;
+    int length = 0;
+
+    read_in_field("Please enter a course name: ", search_name, 51);
+    tree.display_course(search_name);
+
+    cout << "More to display? (y/n): ";
+    cin >> conf; 
+    cin.ignore(100, '\n');
+  } while (tolower(conf) != 'n');
+
+  cout << "Start of Remove by media name" << endl;
+
+  do {
+    char key[51];
+    read_in_field("Please enter in a media name you want to remove: ", key, 51);
+    tree.remove(key);
+
+    cout << "More to remove? (y/n): ";
+    cin >> conf; 
+    cin.ignore(100, '\n');
+  } while (tolower(conf) != 'n');
+
+  cout << "Now printing tree" << endl;
+  tree.display_all();
+  cout << "Tree Height: " << tree.get_height() << endl;
+
+  cout << "Start of Remove by course name" << endl;
+
+  do {
+    char key[51];
+    read_in_field("Please enter in a course name you want to remove: ", key, 51);
+    tree.remove_course(key);
+
+    cout << "More to remove? (y/n): ";
+    cin >> conf; 
+    cin.ignore(100, '\n');
+  } while (tolower(conf) != 'n');
+
+  cout << "Now printing tree" << endl;
+  tree.display_all();
+  cout << "Tree Height: " << tree.get_height() << endl;
 
   return 0;
 }
