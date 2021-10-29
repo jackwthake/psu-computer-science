fc52be2fe317a9611aacfde79afe812e3e11391f
Jack Thake <jackthake@hotmail.com>
2021-10-29 12:22:27 -0700

    Started work on second version of program 2

---
+++ cs202/prog2/ver_2/assignment.h
@@ -0,0 +1,45 @@
+/*
+ * Jack Thake
+ * Karla Fant
+ * CS 202
+ * assignment.h
+ *
+ * This file holds the definition of the assignment class. This class will represent one single assignment, for
+ * a given course. assignment is dervied from the ABT psu_activity and will manage two pieces of information, 
+ * the assignment's due date as well as weather or not the assignment has been finished or not.
+*/
+
+#ifndef __ASSIGNMENT_H__
+#define __ASSIGNMENT_H__
+
+#include "psu_activity.h"
+#include <string>
+
+/*
+ * This class will represent one single assignment for a given course.
+*/
+class assignment : public psu_activity {
+  public:
+    assignment(void); // default constructor
+    assignment(std::string due_date, const char *name, int time_spent, int priority = 1, bool finsihed = false); // normal constructor
+
+    // overridden virtual functions
+    void display(void) const override;
+    void input(void) override;
+    void attend(void) override;
+    void prepare(void) override;
+
+    void finish(void);
+
+    // we only need getters, there should be no reason to modify an assignment
+    std::string get_due_date(void) const;
+    bool is_finished(void) const;
+    
+    /* comparison operator */
+    bool operator==(const assignment &rhs);
+  private:
+    std::string due_date;
+    bool finished;
+};
+
+#endif
---
+++ cs202/prog2/ver_2/main.cpp
@@ -0,0 +1,7 @@
+#include "in_person_class.h"
+#include "assignment.h"
+#include "remote_assistance.h"
+
+int main(void) {
+  return 0;
+};
