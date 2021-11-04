f2b00391b88ecb4483f12fd434dd7bf8f7ef019b
Jack Thake <jackthake@hotmail.com>
2021-11-03 18:13:42 -0700

    Implemented the remote_assistance class

--- cs202/prog2/ver_3/main.cpp
+++ cs202/prog2/ver_3/main.cpp
@@ -1,12 +1,19 @@
 #include "assignment.h"
 #include "in_person_class.h"
+#include "remote_assistance.h"
 
 
 int main(void) {
   assignment a1("10/9/21", "homework 4", 2);
   in_person_class c1("CS202", "Karla Fant", 202, 6, 1); 
+  remote_assistance r1("12:00pm", "M/F", "Tutoring for CS251", 2, 1);
 
   a1.display();
+  c1.display();
+  r1.display();
+
+  r1.input();
+  r1.display();
 
   return 0;
 };
