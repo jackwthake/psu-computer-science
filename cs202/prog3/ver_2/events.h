f0df0ffbe54268a021c4ec6f4bb7e32d312d79ea
Jack Thake <jackthake@hotmail.com>
2021-11-11 13:35:43 -0800

    Fixed issues in the OO structure of the events

--- cs202/prog3/ver_2/events.h
+++ cs202/prog3/ver_2/events.h
@@ -58,24 +58,18 @@ class event {
     bool add_review(std::string review) throw(std::bad_alloc);
     void display_reviews(void) throw(std::out_of_range); // thrown if the list is empty
 
-    /* getters + setters */
-    char *get_name();
-    int get_capacity();
-    int get_length();
-    float get_ticket_price();
-
-    void set_name(const char *);
-    void set_capacity(int cap);
-    void set_length(int len);
-    void set_ticket_price();
-
     /* overloaded operators */
     void operator=(const event &rhs);
     bool operator==(const event &rhs) const;
     bool operator!=(const event &rhs) const;
+
+    bool operator<(const event &rhs) const;
+    bool operator>(const event &rhs) const;
+    bool operator<=(const event &rhs) const;
+    bool operator>=(const event &rhs) const;
     
     friend std::ostream &operator<<(std::ostream &output, const event &obj);
-    friend std::istream &operator<<(std::istream &output, const event &obj);
+    friend std::istream &operator>>(std::istream &intput, event &obj);
   private:
     char *event_name;
     int capacity, length;
@@ -103,15 +97,17 @@ class petting : public event {
     void display_all_animals() const throw(std::out_of_range); // thrown if the list is empty
 
     /* overloaded operators */
-    void operator=(const petting &rhs);
     bool operator==(const petting &rhs) const;
     bool operator!=(const petting &rhs) const;
 
     petting &operator+=(const animal_type &); /* will append an animal to the animal list */
     petting &operator-=(const animal_type &); /* will remove an animal from the animal list */
+
+    friend petting &operator+(const petting &a, const petting &b); /* add two petting events together, combining their lists */
+    friend petting &operator-(const petting &a, const petting &b); /* subtract two pettting events from eachother, removing their matching list elements */
     
     friend std::ostream &operator<<(std::ostream &output, const petting &obj);
-    friend std::istream &operator<<(std::istream &output, const petting &obj);
+    friend std::istream &operator>>(std::istream &intput, petting &obj);
   private:
     std::list<animal_type> animals;
 };
@@ -130,20 +126,22 @@ class aquatic : public event {
     aquatic(const char *name, int capacity, int length, float ticket_price);
 
     /* interact with the exhibit list */
-    bool add_exhibit(std::string exhibit_name, animal_type t) throw(std::bad_alloc);
-    std::pair<std::string, animal_type> &get_exhibit(std::string name) const throw(std::out_of_range); // thrown if the list is empty
+    bool add_exhibit(std::string &exhibit_name, animal_type t) throw(std::bad_alloc);
+    std::pair<std::string, animal_type> &get_exhibit(std::string &name) const throw(std::out_of_range); // thrown if the list is empty
     void display_exhibits() const throw(std::out_of_range); // thrown if the list is empty
     
     /* overloaded operators */
-    void operator=(const aquatic &rhs);
     bool operator==(const aquatic &rhs) const;
     bool operator!=(const aquatic &rhs) const;
 
     aquatic &operator+=(const std::pair<std::string, animal_type> &); /* will append a pair to the exhibit list */
     aquatic &operator-=(const std::pair<std::string, animal_type> &); /* will remove a pair from the exhibit list */
+
+    friend aquatic &operator+(const aquatic &a, const aquatic &b); /* add two aquatic events together, combining their lists */
+    friend aquatic &operator-(const aquatic &a, const aquatic &b); /* subtract two pettting events from eachother, removing their matching list elements */
     
     friend std::ostream &operator<<(std::ostream &output, const aquatic &obj);
-    friend std::istream &operator<<(std::istream &output, const aquatic &obj);
+    friend std::istream &operator>>(std::istream &intput, aquatic &obj);
   private:
     /* list of pairs, containing the name of the exhibit, and the animal type associated with that exhibit */
     std::list<std::pair<std::string, animal_type>> exhibits;
@@ -160,29 +158,27 @@ class safari : public event {
   public:
     /* constructors */
     safari();
-    safari(const char *name, std::string guide, int capacity, int length, float ticket_price); 
+    safari(const char *name, std::string &guide, int capacity, int length, float ticket_price); 
 
     /* interact with the region list */
     bool add_region(std::pair<std::string, std::list<animal_type>> &region) throw(std::bad_alloc);
-    std::pair<std::string, std::list<animal_type>> &construct_region(std::string, int *types, size_t length) throw(std::length_error, std::invalid_argument);
+    std::pair<std::string, std::list<animal_type>> &construct_region(std::string &, int *types, size_t length) throw(std::length_error, std::invalid_argument);
 
-    void display_region(std::string region_name) const throw(std::out_of_range); // thrown if the list is empty
+    void display_region(std::string &region_name) const throw(std::out_of_range); // thrown if the list is empty
     void display_all_regions() const throw(std::out_of_range); // thrown if the list is empty
     
-    /* getters and setters */
-    std::string &get_guide() const;
-    void set_guide(std::string &);
-    
     /* overloaded operators */
-    void operator=(const safari &rhs);
     bool operator==(const safari &rhs) const;
     bool operator!=(const safari &rhs) const;
 
     safari &operator+=(const std::pair<std::string, std::list<animal_type>> &); /* will append a region onto the list */
     safari &operator-=(const std::pair<std::string, std::list<animal_type>> &); /* will remove a region from the list */
+
+    friend safari &operator+(const safari &a, const safari &b); /* add two safari events together, combining their lists */
+    friend safari &operator-(const safari &a, const safari &b); /* subtract two pettting events from eachother, removing their matching list elements */
     
     friend std::ostream &operator<<(std::ostream &output, const safari &obj);
-    friend std::istream &operator<<(std::istream &output, const safari &obj);
+    friend std::istream &operator>>(std::istream &intput, safari &obj);
   private:
     std::string guide;
 
--- cs202/prog3/ver_2/main.cpp
+++ cs202/prog3/ver_2/main.cpp
@@ -1,3 +1,5 @@
+#include "events.h"
+#include "scheduler.h"
 
 int main(void) {
   return 0;
