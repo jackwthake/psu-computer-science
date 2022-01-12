15c48961762c41a4cb70715ca65d29cf2902ed2c
Jack Thake <jackthake@hotmail.com>
2022-01-12 11:30:18 -0800

    Updated to supplied MakeFile

--- CS 201/A1/typing_word_game.c
+++ CS 201/A1/typing_word_game.c
@@ -10,13 +10,14 @@ static const char *words[WORD_LENGTH] = { "The", "quick", "brown", "fox", "jumps
 
 int main(int argc, char **argv) {
     int has_visited[WORD_LENGTH] = { 0 }, i;        /* this array will be used as a boolean array for whether a word ahs already been typed */
-    char buf[10] = { 0 }, c;
+    char buf[10] = { 0 };
     struct timeval start_time, end_time;            /* these will be used to measure the time the user takes */
 
     srand(time(NULL));                              /* initialise the random number generator */
     assert(gettimeofday(&start_time, NULL) == 0);   /* get the starting time for the timer */
 
     for (i = 0; i < WORD_LENGTH; ++i) {
+        char c;
         int curr_word = rand() % WORD_LENGTH;       /* get a random index in the words array */
         while (has_visited[curr_word] == 1)         /* keep generating new indexes until an unused one is found */
             curr_word = rand() % WORD_LENGTH;
@@ -27,7 +28,7 @@ int main(int argc, char **argv) {
         while (strncmp(buf, words[curr_word], strlen(words[curr_word])) != 0) {
             printf("%s: ", words[curr_word]);       /* print out the word to type, and except the appropriately sized input */
             scanf("%10s", buf);
-            while((c = getchar()) != '\n' && c != EOF);
+            while((c = getchar()) != '\n' && c != EOF); /* consume unused characters */
         }
 
         memset(buf, 0x00, 10);                      /* without this, we could get with a different set of words false match */
