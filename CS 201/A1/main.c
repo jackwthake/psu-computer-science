ef0ba60fbf6ce9c679baac8a340d873eeb886f1f
Jack Thake <jackthake@hotmail.com>
2022-01-11 17:59:02 -0800

    Program largely functional

--- CS 201/A1/main.c
+++ CS 201/A1/main.c
@@ -1,8 +1,8 @@
+#include <sys/time.h>
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
-
-#define UNUSED(x) (void)(x)
+#include <string.h>
+#include <assert.h>
 
 /* contains all of the words that the program will ask the user to type */
 #define WORD_LENGTH 9
@@ -10,20 +10,33 @@ static const char *words[WORD_LENGTH] = { "The", "quick", "brown", "fox", "jumps
 
 int main(int argc, char **argv) {
     int has_visited[WORD_LENGTH] = { 0 }, i;
+    char buf[10] = { 0 }, c;
+    struct timeval start_time, end_time;
 
-    UNUSED(argc);
-    UNUSED(argv);
     srand(time(NULL));
+    assert(gettimeofday(&start_time, NULL) == 0);
 
-    for (i = 0; i < 9; ++i) {
-        int word = rand() % WORD_LENGTH;
-        while (has_visited[word] == 1)
-            word = rand() % WORD_LENGTH;
+    for (i = 0; i < WORD_LENGTH; ++i) {
+        int curr_word = rand() % WORD_LENGTH;
+        while (has_visited[curr_word] == 1)
+            curr_word = rand() % WORD_LENGTH;
         
-        has_visited[word] = 1;
-        
-        printf("%s\n", words[word]);
+        has_visited[curr_word] = 1;
+
+        while (strncmp(buf, words[curr_word], strlen(words[curr_word])) != 0) {
+            printf("%s: ", words[curr_word]);
+            scanf("%10s", buf);
+            while((c = getchar()) != '\n' && c != EOF);
+        }
+
+        memset(buf, 0x00, 10);
     }
+
+    assert(gettimeofday(&end_time, NULL) == 0);
+    end_time.tv_sec -= start_time.tv_sec;
+    end_time.tv_usec -= start_time.tv_usec;
+
+    printf("Total time taken: %ld.%06d seconds\n", end_time.tv_sec, end_time.tv_usec);
 
     return 0;
 }
