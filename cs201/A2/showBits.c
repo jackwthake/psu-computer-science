c31a28d86751fcf59ace844ec3f9a27bf9d4b420
Jack Thake <jackthake@hotmail.com>
2022-01-31 14:24:55 -0800

    Refactored Code for final turn in.

--- cs201/A2/showBits.c
+++ cs201/A2/showBits.c
@@ -1,48 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
 #include <stdint.h>
 #include <assert.h>
 
-/* 
- * this function wraps the normal printf and error checks.
- * Variadic argument list from printf.c from:
- * https://code.woboq.org/userspace/glibc/stdio-common/printf.c.html
-*/
-void wrapped_printf(const char *fmt, ...) {
-    va_list args;
-
-    va_start(args, fmt);
-    assert(vprintf(fmt, args) > 0);
-    va_end(args);
-}
-
 int main(int argc, char **argv) {
-    int i;
-    uint32_t a, b, result;
+    uint32_t i, a, b, result;
     char *end;
 
-    if (argc != 3) {
-        wrapped_printf("Error: please provide two hexadecimal numbers as arguments to the program.\n");
+    if (argc != 3) { /* ensure correct amount of input */
+        assert(printf("Error: please provide two hexadecimal numbers as arguments to the program.\n") > 0);
         return -1;
     }
 
+    /* convert arguments to a workable format */
     a = strtol(argv[1], &end, 16);
-    assert(end != NULL);
+    assert(end != argv[1]);
     b = strtol(argv[2], &end, 16);
-    assert(end != NULL);
+    assert(end != argv[2]);
 
+    /* compare the two numbers, the result will have their shared bits */
     result = a & b;
-    wrapped_printf("Bits ");
-    if (result) {
-        for (i = 0; i < 32; ++i) {
-            if (result >> i & 0x1) {
-                wrapped_printf("%d,", i);
-            }
+    assert(printf("Bits ") > 0);
+    for (i = 0; i < 32; ++i) { /* iterate to identify which bits are in common */
+        if (result >> i & 0x1) {
+            assert(printf("%d,", i) > 0);
         }
     }
 
-    wrapped_printf(" in common. Hexadecimal: %x, Signed: %d, Unsigned: %u.\n", result, result, result);
+    assert(printf(" in common. Hexadecimal: %x, Signed: %d, Unsigned: %u.\n", result, result, result) > 0);
 
     return 0;
 }
